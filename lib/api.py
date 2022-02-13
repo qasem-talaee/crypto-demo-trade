d19e0dfa0cf796ab31cbcd52b9de80690f752526
qasem talaee <qasem.talaee1375@gmail.com>
2022-02-13 09:20:56 +0330

    fix api

--- lib/api.py
+++ lib/api.py
@@ -1,29 +1,36 @@
+from sys import flags
 import requests
 import time
 
 class API:
     
     def historical(self, market, start_date):
-        now = time.time()
-        try:
-            data = requests.get("https://api.coingecko.com/api/v3/coins/{market}/market_chart/range?vs_currency=usd&from={start_time}&to={now}".format(market=market, start_time=start_date, now=now))
-        except:
-            return 0
-        else:
-            data = data.json()['prices']
-            result = []
-            for i in range(len(data)):
-                result.append(float(data[i][1]))
-            return result
+        flag = True
+        while flag:
+            now = time.time()
+            try:
+                data = requests.get("https://api.coingecko.com/api/v3/coins/{market}/market_chart/range?vs_currency=usd&from={start_time}&to={now}".format(market=market, start_time=start_date, now=now))
+            except:
+                print('Try again')
+            else:
+                data = data.json()['prices']
+                result = []
+                for i in range(len(data)):
+                    result.append(float(data[i][1]))
+                flag = False
+                return result
     
     def get_kline(self, market):
-        try:
-            data = requests.get("https://api.coingecko.com/api/v3/simple/price?ids={market}&vs_currencies=usd".format(market=market))
-        except:
-            return 0
-        else:
-            data = data.json()
-            return data[market]['usd']    
+        flag = True
+        while flag:
+            try:
+                data = requests.get("https://api.coingecko.com/api/v3/simple/price?ids={market}&vs_currencies=usd".format(market=market))
+            except:
+                print('Try again')
+            else:
+                data = data.json()
+                flag = False
+                return data[market]['usd']
     
     def get_list(self):
         try:
